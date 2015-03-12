ba4c49b09090dd9c375eabcf8ba5a0dba4d55976
minikomi <nerdfunk@gmail.com>
2015-03-12 16:15:55 +0900

    try 'pulling ahead' until :end tag for multilines. broke value tests.

--- src/jughead/core.clj
+++ src/jughead/core.clj
@@ -27,6 +27,21 @@
     (archie-assoc m k (archie-assoc-in (get m k) ks v))
     (archie-assoc m k v)))
 
+(defn generate-keygroup-and-value [initial-line-data remain]
+  (let [[kg initial-value] (transform initial-line-data)]
+    (loop [buffer [initial-value] new-remain remain]
+      (if (empty? new-remain) [kg initial-value (rest remain)]
+        (let [[line-type line-data] (first new-remain)]
+          (if (= :Special line-type)
+            (if (= :End (-> line-data first))
+              [kg (clojure.string/join "\n" buffer) (rest new-remain)]
+              [kg initial-value (rest remain)])
+            (recur (conj buffer (second line-data))
+                   (rest new-remain))))))))
+
+(defn special-tag-isnt [tag]
+  #(not (= tag (-> % second first))))
+
 (defn interpret [parsed]
   (loop [remain parsed
          result {}]
@@ -37,11 +52,11 @@
           :Normal (recur (rest remain) result)
           :Special (case (first line-data)
                      :KeyValuePair 
-                     (recur (rest remain)
-                            (let [[kg v] (transform line-data)]
+                     (let [[kg v new-remain] (generate-keygroup-and-value line-data (rest remain))]
+                       (recur new-remain
                               (archie-assoc-in result kg v)))
                      :Skip 
-                     (recur (rest (drop-while #(not (= :EndSkip (-> % second first))) remain))
+                     (recur (rest (drop-while (special-tag-isnt :EndSkip) remain))
                             result)
                      :Ignore
                      result
